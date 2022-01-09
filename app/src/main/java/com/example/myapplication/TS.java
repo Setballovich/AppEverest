fd4a3ce2d5d103c1a531579f60533f23b67b6476
Илья <ilya_pavlo@list.ru>
2022-01-09 20:05:21 +0500

    Create TS.java

---
+++ app/src/main/java/com/example/myapplication/TS.java
@@ -0,0 +1,100 @@
+package com.example.myapplication;
+
+import android.telephony.TelephonyManager;
+
+public class TS {
+
+        public static String callState(Integer state) {
+            switch (state) {
+                case TelephonyManager.CALL_STATE_IDLE:
+                    return "IDLE";
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    return "OFFHOOK";
+                case TelephonyManager.CALL_STATE_RINGING:
+                    return "RINGING";
+                default:
+                    return "UNDEFINED";
+            }
+        }
+
+        public static String phoneType(Integer type) {
+            switch (type) {
+                case TelephonyManager.PHONE_TYPE_CDMA:
+                    return "CDMA";
+                case TelephonyManager.PHONE_TYPE_GSM:
+                    return "GSM";
+                case TelephonyManager.PHONE_TYPE_NONE:
+                    return "NONE";
+                default:
+                    return "UNDEFINED";
+            }
+        }
+
+        public static String networkType(Integer type) {
+            switch (type) {
+                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                    return "NONE";
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                    return "GPRS";
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                    return "EDGE";
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                    return "UMTS";
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                    return "CDMA";
+                default:
+                    return "UNDEFINED";
+            }
+        }
+
+        public static String dataState(Integer state) {
+            switch (state) {
+                case TelephonyManager.DATA_DISCONNECTED:
+                    return "DISCONNECTED";
+                case TelephonyManager.DATA_CONNECTING:
+                    return "CONNECTING";
+                case TelephonyManager.DATA_CONNECTED:
+                    return "CONNECTED";
+                case TelephonyManager.DATA_SUSPENDED:
+                    return "SUSPENDED";
+                default:
+                    return "UNDEFINED";
+            }
+        }
+    public static String dataActivity(Integer act) {
+        switch (act) {
+            case TelephonyManager.DATA_ACTIVITY_NONE:
+                return "NONE";
+            case TelephonyManager.DATA_ACTIVITY_IN:
+                return "IN";
+            case TelephonyManager.DATA_ACTIVITY_OUT:
+                return "OUT";
+            case TelephonyManager.DATA_ACTIVITY_INOUT:
+                return "INOUT";
+            case TelephonyManager.DATA_ACTIVITY_DORMANT:
+                return "DORMANT";
+            default:
+                return "UNDEFINED";
+        }
+    }
+
+    public static String simState(Integer state) {
+        switch (state) {
+            case TelephonyManager.SIM_STATE_UNKNOWN:
+                return "UNKNOWN";
+            case TelephonyManager.SIM_STATE_ABSENT:
+                return "ABSENT";
+            case TelephonyManager.SIM_STATE_PIN_REQUIRED:
+                return "PIN REQUIRED";
+            case TelephonyManager.SIM_STATE_PUK_REQUIRED:
+                return "PUK REQUIRED";
+            case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
+                return "NETWORK LOCKED";
+            case TelephonyManager.SIM_STATE_READY:
+                return "READY";
+            default:
+                return "UNDEFINED";
+
+        }
+    }
+}
