10fac1cb95ad4b5fa17cd8d84ed2fe6290680356
Илья <91188698+Nikita123456ghjb@users.noreply.github.com>
2022-01-09 17:10:50 +0500

    Переход на SecondActivity
    Добавил переход на второй экран

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private void showSecondActivityWithDelay() {
+        Handler handler = new Handler( );
+        handler.postDelayed(() -> startActivity(new Intent(MainActivity.this, SecondActivity.class)), 2000);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart( );
+        showSecondActivityWithDelay( );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
