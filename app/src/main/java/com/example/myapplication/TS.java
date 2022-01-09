3fa3cc465c82ef5b3bb3169a7e13ad1f9f1e2c47
Илья <91188698+Nikita123456ghjb@users.noreply.github.com>
2022-01-09 21:19:41 +0500

    Update TS.java
    обновил информацию в классе "TS" , что касаемо батареи

--- app/src/main/java/com/example/myapplication/TS.java
+++ app/src/main/java/com/example/myapplication/TS.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.os.BatteryManager;
 import android.telephony.TelephonyManager;
 
 public class TS {
@@ -97,4 +98,50 @@ public class TS {
 
         }
     }
+    public static String batteryHealth(int health) {
+        switch (health) {
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                return "DEAD";
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                return "GOOD";
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                return "OVERHEAT";
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                return "OVER VOLTAGE";
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                return "UNKNOWN";
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                return "UNSPECIFIED FAILURE";
+            default:
+                return "UNDEFINED";
+
+        }
+    }
+    public static String batteryPlugged (Integer plug) {
+        switch (plug) {
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                return "AC";
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                return "USB";
+            default:
+                return "UNDEFINED";
+        }
+    }
+    public static String batteryStatus (Integer status) {
+        switch (status) {
+            case BatteryManager.BATTERY_STATUS_CHARGING:
+                return "CHARGING";
+            case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                return "DISCHARGING";
+            case BatteryManager.BATTERY_STATUS_FULL:
+                return "FULL";
+            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                return "NOT CHARGING";
+            case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                return "UNKNOWN";
+            default:
+                return "UNDEFINED";
+        }
+    }
+
 }
