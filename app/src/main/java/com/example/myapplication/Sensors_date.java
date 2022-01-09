e4585ab79130f28c0d8c5021b5cceddc5f83609a
Илья <92714353+shikiteyka@users.noreply.github.com>
2022-01-09 23:28:23 +0500

    Create Sensors_date.java
    Co-Authored-By: Илья  <91187904+Setballovich@users.noreply.github.com>

---
+++ app/src/main/java/com/example/myapplication/Sensors_date.java
@@ -0,0 +1,152 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+
+public class Sensors_date extends AppCompatActivity implements SensorEventListener {
+    private static final String TAG = "Light_sensor";
+
+    private SensorManager sensorManager;
+
+    private Sensor accelerometr,mGyro,mMagno,mLight,mPressure,mTemp,mHumi;
+
+
+    TextView xValue,yValue,zValue,xGyroValue,yGyroValue,zGyroValue, xMagnoValue, yMagnoValue,zMagnoValue,light,pressure,temp,humi;
+
+    @Override
+    protected void onCreate (Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.activity_sensors_date);
+
+        xValue = (TextView) findViewById(R.id.xValue);
+        yValue = (TextView) findViewById(R.id.yValue);
+        zValue = (TextView) findViewById(R.id.zValue);
+
+        xGyroValue = (TextView) findViewById(R.id.xGyroValue);
+        yGyroValue = (TextView) findViewById(R.id.yGyroValue);
+        zGyroValue = (TextView) findViewById(R.id.zGyroValue);
+
+        xMagnoValue = (TextView) findViewById(R.id.xMagnoValue);
+        yMagnoValue = (TextView) findViewById(R.id.yMagnoValue);
+        zMagnoValue = (TextView) findViewById(R.id.zMagnoValue);
+
+        light = (TextView) findViewById(R.id.light);
+        pressure = (TextView) findViewById(R.id.pressure);
+        temp = (TextView) findViewById(R.id.temp);
+        humi = (TextView) findViewById(R.id.humi);
+
+        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        sensorManager = (SensorManager) getSystemService((Context.SENSOR_SERVICE));
+
+        accelerometr = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometr !=null) {
+            sensorManager.registerListener(Sensors_date.this,accelerometr,SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG, "onCreate: Registered accelerometer listener");
+        }else {
+            xValue.setText("Accelerometer Not Supported ");
+            yValue.setText("Accelerometer Not Supported ");
+            zValue.setText("Accelerometer Not Supported ");
+
+        }
+
+        mGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (mGyro !=null) {
+            sensorManager.registerListener(Sensors_date.this,mGyro,SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG, "onCreate: Registered Gyro listener");
+        }else {
+            xGyroValue.setText("Gyro Not Supported ");
+            yGyroValue.setText("Gyro Not Supported ");
+            zGyroValue.setText("Gyro Not Supported ");
+        }
+
+        mMagno = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (mMagno !=null) {
+            sensorManager.registerListener(Sensors_date.this,mMagno,SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG, "onCreate: Registered Magnometer listener");
+        }else {
+            xMagnoValue.setText("Magno Not Supported ");
+            yMagnoValue.setText("Magno Not Supported ");
+            zMagnoValue.setText("Magno Not Supported ");
+        }
+
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (mLight !=null) {
+            sensorManager.registerListener(Sensors_date.this,mLight,SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG, "onCreate: Registered light listener");
+        }else {
+            light.setText("light Not Supported ");
+        }
+
+        mPressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if (mPressure !=null) {
+            sensorManager.registerListener(Sensors_date.this,mPressure,SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG, "onCreate: Registered Pressure listener");
+        }else {
+            pressure.setText("Pressure Not Supported ");
+        }
+
+        mTemp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if (mTemp !=null) {
+            sensorManager.registerListener(Sensors_date.this,mTemp,SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG, "onCreate: Registered Temp listener");
+        }else {
+            temp.setText("Temp Not Supported ");
+        }
+
+        mHumi = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        if (mHumi !=null) {
+            sensorManager.registerListener(Sensors_date.this,mHumi,SensorManager.SENSOR_DELAY_NORMAL);
+            Log.d(TAG, "onCreate: Registered Humi listener");
+        }else {
+            humi.setText("humi Not Supported ");
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+        if (sensor.getType( ) == Sensor.TYPE_ACCELEROMETER) {
+            Log.d(TAG, "OnSensorChanged: X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
+
+            xValue.setText("xValue " + sensorEvent.values[0]);
+            yValue.setText("yValue " + sensorEvent.values[1]);
+            zValue.setText("zValue " + sensorEvent.values[2]);
+        } else if (sensor.getType()== Sensor.TYPE_GYROSCOPE) {
+            xGyroValue.setText("xGValue " + sensorEvent.values[0]);
+            yGyroValue.setText("yGValue " + sensorEvent.values[1]);
+            zGyroValue.setText("zGValue " + sensorEvent.values[2]);
+        } else if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            xMagnoValue.setText("xMValue " + sensorEvent.values[0]);
+            yMagnoValue.setText("yMValue " + sensorEvent.values[1]);
+            zMagnoValue.setText("zMValue " + sensorEvent.values[2]);
+        }else if(sensor.getType() == Sensor.TYPE_LIGHT) {
+            light.setText("Light: " + sensorEvent.values[0]);
+        }else if(sensor.getType() == Sensor.TYPE_PRESSURE) {
+            pressure.setText("Pressure: " + sensorEvent.values[0]);
+        }else if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            temp.setText("Temp: " + sensorEvent.values[0]);
+        }else if (sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY) {
+            humi.setText("Humidity: " + sensorEvent.values[0]);
+        }
+
+    }
+}
+
